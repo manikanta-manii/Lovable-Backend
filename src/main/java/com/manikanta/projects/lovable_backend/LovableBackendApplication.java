9706eba4650689307e2d3d3ae94ab047473f622e
Manikanta Parameswarappa <mparameswarappa@plansource.com>
2025-12-13 11:55:27 +0530

    project-init

---
+++ src/main/java/com/manikanta/projects/lovable_backend/LovableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manikanta.projects.lovable_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovableBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovableBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manikanta/projects/lovable_backend/LovableBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manikanta.projects.lovable_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovableBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
